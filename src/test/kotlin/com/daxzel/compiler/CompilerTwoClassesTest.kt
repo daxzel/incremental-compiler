339f49ba6ad406ace71a36dd7c8cd9d0b38537de
Andrey Tsarevskiy <atcarevs@amazon.com>
2021-02-22 23:59:20 +0000

    Add a list of of use casses for two classes

--- src/test/kotlin/com/daxzel/compiler/CompilerTwoClassesTest.kt
+++ src/test/kotlin/com/daxzel/compiler/CompilerTwoClassesTest.kt
@@ -4,16 +4,19 @@ import org.junit.jupiter.api.Test
 
 class CompilerTwoClassesTest {
 
+    // No dependency between class
+
     @Test
     fun testCase1() {
-        // No dependency between classes.
-        // A new method is added
+        // A new method has been added.
         incrementalCompilationTest(TWO_CLASSES_GROUP, 1, 2, 1)
     }
 
+    // There is a dependency from one class to another.
+
     @Test
     fun testCase2() {
-        // There is a dependency from one class to another. A new static method has been added.
+        // A new static method has been added.
         // TODO: there is a possibility to avoid rebuilding dependencies if it is only the body change
         incrementalCompilationTest(TWO_CLASSES_GROUP, 2, 2, 2)
     }
@@ -21,7 +24,31 @@ class CompilerTwoClassesTest {
     @Test
     fun testCase3() {
         // There is a dependency from one class to another. A method which used by another class has been removed
-        incrementalCompilationTest(TWO_CLASSES_GROUP, 2, 2, 2)
+        incrementalCompilationTest(TWO_CLASSES_GROUP, 3, 2, 2)
     }
-    
+
+    @Test
+    fun testCase4() {
+        // Both files have been removed
+        incrementalCompilationTest(TWO_CLASSES_GROUP, 4, 2, 0)
+    }
+
+    @Test
+    fun testCase5() {
+        // Both files have been added
+        incrementalCompilationTest(TWO_CLASSES_GROUP, 5, 0, 2)
+    }
+
+    @Test
+    fun testCase6() {
+        // The main class (on which other depends) has been removed
+        incrementalCompilationTest(TWO_CLASSES_GROUP, 6, 2, 1)
+    }
+
+    @Test
+    fun testCase7() {
+        // The main class (on which other depends) has been added
+        incrementalCompilationTest(TWO_CLASSES_GROUP, 7, 1, 2)
+    }
+
 }
---
+++ src/test/resources/com/daxzel/compiler/usecases/twoclasses/case4/before/test/Main.java
@@ -0,0 +1,12 @@
+package test;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void test() {
+        System.out.println("test");
+    }
+}
